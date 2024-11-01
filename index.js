9f201ad225057c72221ff58fb0d0b094e178477c
Zakkin <99321094+Zakkin@users.noreply.github.com>
2024-11-01 23:33:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🌙""];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["moon🌙"];
+const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
